5b91d4d502cddbb24c78e5a9895f073e339a40fc
Nicklas Agerlin Andersen <nick4453@VID.net.local>
2016-03-09 13:50:03 +0100

    Added scene "Nicklas" Added Script "Respawn" to scripts Added temporary checkpoints to placeholder Added "deathspikes" to placeholders

---
+++ Twinstick Platformer/Assets/Scripts/Respawn.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour 
+{
+	
+    public GameObject savedCheckpoint;
+    public float lowerDeath = 4;
+    public bool playerMortal;
+    public float deathDelay;
+    float deathTimer;
+// Use this for initialization
+	void Start () 
+    {
+        //Sætter startvectoren til checkpointed
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (transform.position.y < lowerDeath)
+        {
+            Die();
+        }
+        if (!playerMortal)
+        {
+            deathTimer += Time.deltaTime;
+            if (deathTimer > deathDelay)
+            {
+                playerMortal = true;
+                deathTimer = 0;
+            }
+
+        }
+	
+	}
+
+    void Die()
+    {
+        if (playerMortal)
+        {
+            transform.position = savedCheckpoint.transform.position;
+            playerMortal = false;
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) //Tjekker for at se om gameobjekted kolliderer med et bestemt andet objekt
+    {
+        // Tjekker hvilket type objekt der kollideres med
+        if (other.tag == "CheckPoint")
+        {
+            //Ændre positionen på den nyeste vectorposition til at være den samme som gameobjektet
+            savedCheckpoint = other.gameObject;
+           // GameVariables.newestCheckPoint = transform.position;
+            Debug.Log("Saved");
+
+
+        }
+
+        if (other.tag == "Trap")
+        {
+            //Ændre positionen på det kolliderende objekt til den sidste registrede checkpoint possition
+            Die();
+            Debug.Log("Trigger");
+        }
+    }
+}
