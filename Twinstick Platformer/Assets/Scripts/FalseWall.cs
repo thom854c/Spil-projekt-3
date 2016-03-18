ca217713e4ceebd7b30c3775446d123a4379ba3e
Thomas Pedersen <Thomas.pedersen@stofanet.dk>
2016-03-18 11:00:46 +0100

    Added hidden room with destroyable wall and fade in (Can't e opened by magic yet)

---
+++ Twinstick Platformer/Assets/Scripts/FalseWall.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FalseWall : MonoBehaviour
+{
+
+    public float duration = 1.0f;
+
+
+    public GameObject HideWall;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            HideWall.SetActive(false);
+
+        }
+
+    }
+
+
+   public void Update()
+    {
+       if (HideWall.gameObject.active == false)
+       {
+        //GetComponent<SpriteRenderer>().color = new Color(0, 0, 0,  Mathf.SmoothStep(maximum, minimum, t));
+        iTween.FadeTo(gameObject,0f,duration);
+       }
+
+       
+
+    }
+
+
+}
