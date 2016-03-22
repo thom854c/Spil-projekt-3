2e534534818b77bfa74d5b05744ddae3147f5c26
Thomas Pedersen <Thomas.pedersen@stofanet.dk>
2016-03-22 14:33:23 +0100

    Added Boss level with boss. Added more tags. Added platform tiles. Added placeholder missiles for boss.

---
+++ Twinstick Platformer/Assets/Scripts/MissileBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileBehaviour : MonoBehaviour
+{
+
+    public int MissileSpeed;
+
+	void Update () 
+    {
+	    transform.Translate(new Vector3(0,MissileSpeed,0)*Time.deltaTime);
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            StaticVariables.PlayerHealth --;
+            Destroy(gameObject);
+        }
+        else if (other.tag == "Missile" || other.tag == "Boss")
+        {
+           //do nothing 
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
