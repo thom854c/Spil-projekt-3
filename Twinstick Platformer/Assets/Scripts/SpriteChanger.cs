dda8839c2dcb85b50504886ffacf44d1dea49a30
Nicklas Agerlin Andersen <nick4453@VID.net.local>
2016-03-29 13:02:12 +0200

    Added: Checkpoint prefab

---
+++ Twinstick Platformer/Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteChanger : MonoBehaviour 
+{
+    public Sprite checkpointFaded;
+    public Sprite checkpointActive;
+
+    private SpriteRenderer spriteRenderer; 
+	// Use this for initialization
+	void Start () 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer.sprite == null)
+            spriteRenderer.sprite = checkpointFaded;
+	}
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            ChangeSprite();
+            Debug.Log("gotcha");
+        }
+    }
+
+    void ChangeSprite()
+    {
+        if (spriteRenderer.sprite == checkpointFaded)
+        {
+            spriteRenderer.sprite = checkpointActive;
+        }
+
+    }
+	// Update is called once per frame
+	void Update () 
+    {
+
+	
+	}
+}
