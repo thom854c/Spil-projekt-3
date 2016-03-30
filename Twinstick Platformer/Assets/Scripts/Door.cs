2d1b4c5416bb2ac696140781e3141f97c05911d0
unknown <jona669d@VID3C970EAEFF70.djes.dk>
2016-03-30 15:43:40 +0200

    Door, Chandelier, lvl Kadoh Improvement

---
+++ Twinstick Platformer/Assets/Scripts/Door.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D other) 
+    {
+	    if (other.tag == "Player")
+	    {
+            Debug.Log("'NextLvlWorksMaybe' - Avokadoh");
+	        Application.LoadLevel(Application.loadedLevel + 1);
+	    }
+	}
+}
--- Twinstick Platformer/Assets/Scripts/UI/CrystalsScript.cs
+++ Twinstick Platformer/Assets/Scripts/UI/CrystalsScript.cs
@@ -33,12 +33,6 @@ public class CrystalsScript : MonoBehaviour
 	// Use this for initialization
 	void Start () 
     {
-        Player = GameObject.FindGameObjectWithTag("Player");
-        if (Player.name == "Player (clone)")
-        {
-            Destroy(Player);
-            Player = GameObject.FindGameObjectWithTag("Player");            
-        }
         CrystalsCircleRadius = Screen.width/14;
         //Max_Mana = GetComponent<Player>().MaxMana;
         //Cur_Mana = GetComponent<Player>().curMana;
