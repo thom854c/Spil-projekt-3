ab48c2c9bebae1262c73f332f1d40a1343394c3f
Nicklas Agerlin Andersen <nick4453@VID.net.local>
2016-03-30 10:55:47 +0200

    Finished up checkpoint prefab (no sound)

--- Twinstick Platformer/Assets/Scripts/SpriteChanger.cs
+++ Twinstick Platformer/Assets/Scripts/SpriteChanger.cs
@@ -6,7 +6,10 @@ public class SpriteChanger : MonoBehaviour
     public Sprite checkpointFaded;
     public Sprite checkpointActive;
 
-    private SpriteRenderer spriteRenderer; 
+    private SpriteRenderer spriteRenderer;
+
+    private GameObject[] checkPoints;
+
 	// Use this for initialization
 	void Start () 
     {
@@ -30,6 +33,7 @@ public class SpriteChanger : MonoBehaviour
         if (spriteRenderer.sprite == checkpointFaded)
         {
             spriteRenderer.sprite = checkpointActive;
+            DisableOtherCheckPoints();
         }
 
     }
@@ -39,4 +43,21 @@ public class SpriteChanger : MonoBehaviour
 
 	
 	}
+
+    /// <summary>
+    /// Find alle checkpoints og fade deres sprites (undtagen denne instance);
+    /// </summary>
+    void DisableOtherCheckPoints()
+    {
+        string _curCheckPointName = transform.name;
+        //Debug.Log(_curCheckPointName);
+        checkPoints = GameObject.FindGameObjectsWithTag("CheckPoint");
+        foreach (GameObject cp in checkPoints){
+            if (cp.transform.name != _curCheckPointName)
+            {
+                cp.GetComponent<SpriteRenderer>().sprite = checkpointFaded;
+               // Debug.Log(cp.transform.name);
+            }
+        }
+    }
 }
